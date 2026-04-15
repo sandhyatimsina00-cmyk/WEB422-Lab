a5b7fae9cb9ebb17354f027d0ee7b02179390580
Sandhya Timsina <sandhyatimsina00@gmail.com>
2026-04-15 17:24:02 -0400

    added fix

--- web422-a2/user-api/server.js
+++ web422-a2/user-api/server.js
@@ -32,7 +32,14 @@ passport.use(
 );
 
 const app = express();
-app.use(cors());
+const corsOptions = {
+  origin: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json());
 app.use(passport.initialize());
 
